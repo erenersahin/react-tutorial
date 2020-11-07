2217ef0f33a247f23c99051e1cb02115a29dda4f
Eren Ersahin <ersahineren@gmail.com>
2020-11-07 23:27:39 +0300

    Using state of Component.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,24 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Gül', age: 56},
+      {name: 'Eren', age: 25},
+      {name: 'Kübra', age: 24}
+    ]
+  }
+
+
   render() {
     return (
       <div className="App">
        <h1>Hi, I'm a React App</h1>
        <p>This is really working!</p>
-       <Person name="Gül" age="56" />
-       <Person name="Eren" age="25" >My Hobbies: Music Production</Person>
-       <Person name="Kübra" age="24" />
+       <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Music Production</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null,'Hi, I\'m a React App!!!!'))
